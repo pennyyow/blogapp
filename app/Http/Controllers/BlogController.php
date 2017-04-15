3ecb5861354e1ec498225a3a48794d0d363ae1bb
201301144 <201301144@iacademy.edu.ph>
2017-04-15 19:18:34 +0800

    added changes for like-dislike-comment.

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,6 +8,7 @@ use Input;
 use App\Blogs;
 use App\Profile;
 use DB;
+use Carbon\Carbon;
 
 class BlogController extends Controller
 {
@@ -18,12 +19,19 @@ class BlogController extends Controller
     }
 
     public function home(){
-    	return view('blogs.posts');
+        $blogs = Blogs::all();
+    	return view('blogs.posts', compact('blogs'));
     }
 
-    public function profile(){
-        $image = User::get();
-    	return view('blogs.profile', compact('image'));
+    public function profile($id){
+        $user = User::find($id);
+    	return view('blogs.profile')->with([
+            '_id' => $user->_id,
+            'firstName' => $user->firstName,
+            'lastName' => $user->lastName,
+            'email' => $user->email,
+            'image' => $user->image
+        ]);
     }
 
     public function categories(){
@@ -36,6 +44,102 @@ class BlogController extends Controller
 
     public function createBlog(){
         return view('blogs.create-blog');
+    }
+
+    public function deleteBlog() {
+        //delete process
+        return 'deleting blog: '.Input::get('blog'); //magreturn ka dito ng kahit ano;
+    }
+
+    public function editBlog($id) {
+        //render edit page
+        return 'editing blog: ' . $id;
+    }
+
+    public function listBlogs() {
+        $max = (int) Input::get('max');
+        $result = Blogs::orderBy('updated_at', 'desc')->take($max)->get();
+        return [
+            'total' => $result->count(),
+            'blogs' => $result
+        ];
+    }
+
+    public function listBlogsByUser() {
+        $user = Input::get('user');
+        $max = (int) Input::get('max');
+        $res = Blogs::orderBy('updated_at', 'desc')->raw(
+            function($collection) use($user) {
+                return $collection->find([
+                    'user._id' => '58e74331d7da1d1f94006ec2'
+                    ], 
+                    ['sort' => ['updated_at' => -1]]);
+            })->take($max);
+
+        return [
+            'total' => $res->count(),
+            'blogs' => $res
+        ];
+    }
+
+    public function react() {
+        $blogId = Input::get('blog');
+        $reaction = (int) Input::get('reaction');
+
+        $blog = Blogs::find($blogId);
+        $reactions = $blog->reactions ? $blog->reactions : [];
+        
+        $searched = false;
+        $count = 0;
+        foreach ($reactions as $value) { 
+            if($value['_id'] == auth()->user()->_id) {
+                $searched = true;
+                if($value['reaction'] == $reaction) {
+                    array_splice($reactions, $count);
+                    break;
+                } else {
+                    $reactions[$count]['reaction'] = $reaction;
+                    break;
+                }
+            }
+            $count++;
+        }
+
+        if(!$searched) {
+            array_push($reactions, [
+                '_id' => auth()->user()->_id,
+                'reaction' => $reaction
+            ]);
+        }
+
+        $blog->reactions = $reactions;
+        $blog->save();
+
+        return $blog; 
+    }
+
+    public function comment() {
+        $blogId = Input::get('blog');
+        $comment = Input::get('comment');
+
+        $blog = Blogs::find($blogId);
+        $comments = $blog->comments ? $blog->comments : [];
+
+        array_push($comments, [
+            'content' => $comment,
+            'user' => [
+                '_id' => auth()->user()->_id,
+                'image' => auth()->user()->image,
+                'firstName' => auth()->user()->firstName,
+                'lastName' => auth()->user()->lastName
+            ],
+            'dateAdded' => Carbon::now()
+        ]);
+
+        $blog->comments = $comments;
+        $blog->save();
+
+        return $blog;
     }
 
     public function updateProfile(Request $request){
@@ -53,10 +157,7 @@ class BlogController extends Controller
         $updateProfile = Profile::updateProfile($firstName, $lastName, $image);
         return redirect('/profile');
     }
-    /*public function listBlogs(){
-        $blogs = Blogs::all();
-        return redirect('/posts', compact('blogs'));
-    }*/
+    
     public function create(Request $request){
         $categ = Input::get('category');
         if($request->file('file') == null){
@@ -89,17 +190,6 @@ class BlogController extends Controller
                 $avatar = 'img_profile_big';
             }
 
-            $blog = new Blogs();
-
-            $blog->title = Input::get('title');
-            $blog->category = Input::get('category');
-            $blog->tags = $request->tags;
-            $blog->description = Input::get('description');
-            $blog->content = Input::get('content');
-            $blog->image = $avatar;
-            $blog->user = auth()->user();
-            $blog->save();
-
         }else{
 
             $fileImage = $request->file('file');
@@ -108,19 +198,38 @@ class BlogController extends Controller
             //TODO: Change filename of uplodaed thumbnail to id of article
             $avatar = $fileImage->getClientOriginalName();
             $fileImage->move($destination_path, $avatar);
-
-            $blog = new Blogs();
-
-            $blog->title = Input::get('title');
-            $blog->category = Input::get('category');
-            $blog->tags = $request->tags;
-            $blog->description = Input::get('description');
-            $blog->content = Input::get('content');
-            $blog->image = $avatar;
-            $blog->user = auth()->user();
-            $blog->save();
         }
 
+        $blog = new Blogs();
+
+        $blog->title = Input::get('title');
+        $blog->category = Input::get('category');
+        $blog->tags = $request->tags;
+        $blog->description = Input::get('description');
+        $blog->content = Input::get('content');
+        $blog->image = $avatar;
+        $blog->user = [
+            "_id" => auth()->user()->_id,
+            "firstName" => auth()->user()->firstName,
+            "lastName" => auth()->user()->lastName,
+            "email" => auth()->user()->email,
+            "image" => auth()->user()->image
+        ];
+        $blog->save();
+
         return redirect('/posts');
+    }
+
+    public function view($id) {
+        $blog = Blogs::find($id);
+        $views = $blog->views ? $blog->views : 0;
+        $views++;
+        $blog->views = $views;
+        $blog->save();
+        return view('blogs.blog', compact('blog'));
+    }
+
+    public function getBlog() {
+        return Blogs::find(Input::get('blog'));
     }
 }
--- resources/views/blogs/blog.blade.php
+++ resources/views/blogs/blog.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @section('styles')
-    <link href="css/custom/blog.css" rel="stylesheet">
+    <link href="{{ asset('css/custom/blog.css') }}" rel="stylesheet">
 @stop
 <title>BlogMoTo | Blog</title>
 
@@ -10,160 +10,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="ibox">
-                    <div class="ibox-content no-padding border-left-right">
-                        <img alt="image" class="img-responsive" src="img/profile_big.jpg">
-                    </div>
-                    <div class="ibox-content">
-                        
-                        <div class="text-center article-title">
-                            <h1>
-                                Behind the word mountains
-                            </h1>
-                            <span class="text-muted"> 
-                                By <a href="#" class="btn-link">
-                                    <strong>John Patrick Bagacina</strong>
-                                </a>
-                                <i class="fa fa-clock-o"></i> 28th Oct 2017 
-                            </span>
-                        </div>
-                        <p>
-                            Many desktop publishing packages and web page editors now use <strong>Lorem Ipsum as their default model text</strong>, and a search for 'lorem ipsum' will uncover many web
-                        </p>
-                        <p>
-                            One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather. Drops
-                        </p>
-                        <p>
-                            <i>
-                            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-                            </i>
-                        </p>
-                        <p>
-                            The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek,
-                        </p>
-                        <p>
-                            Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk. A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just
-                        </p>
-                        <p>
-                            Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex.
-                        </p>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <h5>Tags:</h5>
-                                    <button class="btn btn-white btn-xs" type="button">
-                                        <i class="fa fa-tag"></i>
-                                        Adventures
-                                        </button>
-                                    <button class="btn btn-white btn-xs" type="button">
-                                        <i class="fa fa-tag"></i>
-                                        Mountain
-                                    </button>
-                                    <button class="btn btn-white btn-xs" type="button">
-                                        <i class="fa fa-tag"></i>
-                                        Dora
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="pull-right status">
-                                    <p>
-                                        <i class="fa fa-thumbs-up"></i> 100 Likes
-                                        &nbsp;&nbsp;&nbsp;<i class="fa fa-thumbs-down"></i> 80 Dislikes
-                                        &nbsp;&nbsp;&nbsp;<i class="fa fa-comments"></i> 50 Comments
-                                    </p>
-                                </div>
-                                <div class="btn-group">
-                                    <button class="btn btn-white btn-xs"><i class="fa fa-thumbs-up"></i> Like
-                                    </button>
-                                    <button class="btn btn-white btn-xs"><i class="fa fa-thumbs-down"></i> Dislike
-                                    </button>
-                                    <button class="btn btn-white btn-xs"><i class="fa fa-comments"></i> Comment</button>
-                                    <button class="btn btn-white btn-xs"><i class="fa fa-share"></i> Share</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="social-feed-box">
-                                    <div class="social-avatar">
-                                        <a href="" class="pull-left">
-                                            <img alt="image" src="img/a1.jpg">
-                                        </a>
-                                        <div class="media-body">
-                                            <a href="#">
-                                                Andrew Williams
-                                            </a>
-                                            <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                        </div>
-                                    </div>
-                                    <div class="social-body">
-                                        <p>
-                                            Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                            default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                            default model text.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="social-feed-box">
-                                    <div class="social-avatar">
-                                        <a href="" class="pull-left">
-                                            <img alt="image" src="img/a2.jpg">
-                                        </a>
-                                        <div class="media-body">
-                                            <a href="#">
-                                                Michael Novek
-                                            </a>
-                                            <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                        </div>
-                                    </div>
-                                    <div class="social-body">
-                                        <p>
-                                            Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                            default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                            default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                            in their infancy. Packages and web page editors now use Lorem Ipsum as their
-                                            default model text.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="social-feed-box">
-                                    <div class="social-avatar">
-                                        <a href="" class="pull-left">
-                                            <img alt="image" src="img/a3.jpg">
-                                        </a>
-                                        <div class="media-body">
-                                            <a href="#">
-                                                Alice Mediater
-                                            </a>
-                                            <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
-                                        </div>
-                                    </div>
-                                    <div class="social-body">
-                                        <p>
-                                            Many desktop publishing packages and web page editors now use Lorem Ipsum as their
-                                            default model text, and a search for 'lorem ipsum' will uncover many web sites still
-                                            in their infancy. Packages and web page editors now use Lorem Ipsum as their
-                                            default model text.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="social-feed-box">
-                                    <div class="social-avatar comment-text">
-                                        <a href="" class="pull-left">
-                                            <img alt="image" src="img/a5.jpg">
-                                        </a>
-                                        <div class="media-body">
-                                            <textarea class="form-control" placeholder="Write comment..." rows="3"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div id="blog"></div>
                 </div>
             </div>
         </div>
@@ -171,3 +18,18 @@
 
 
 @endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        var Url = {
+            react: '{{ url('/react') }}',
+            comment: '{{ url('/comment') }}',
+            getBlog: '{{ !auth()->guest() ? url('/get-blog') : url('/pub-get-blog')  }}'
+        }; 
+
+        var isGuest = ('{{auth()->guest()}}' != '1' ? false : true);
+        var blogId = '{{ $blog->_id }}';
+        var token = '{{ csrf_token() }}';
+    </script>
+    <script src="{{ asset('js/blogs/view.js') }}"></script>
+@stop
